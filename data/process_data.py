64d7d593a8be27d6b2f3332a93f34a7762920989
Charles Beacham <charles.beacham@daimlertruck.com>
2023-12-16 11:59:54 -0800

    initial process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,38 @@
 import sys
-
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
-
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = pd.merge(messages, categories,
+                  how='inner', on='id')
+    
+    return df
 
 def clean_data(df):
-    pass
+    categories = df['categories'].str.split(';', expand=True)
+    category_colnames = list(categories.iloc[0].apply(lambda x: x[:-2]))
+    categories.columns = category_colnames
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].apply(lambda x: x[-1])
+        
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
 
+    df.drop(columns=['categories'], inplace=True)
+    df = pd.concat([df, categories], axis=1)
+    df.drop(df.loc[df.duplicated()].index, inplace=True)
+
+    return df
 
 def save_data(df, database_filename):
-    pass  
+    engine = create_engine(f'sqlite:///{database_filename}')
+    df.to_sql(database_filename, engine, index=False)  
 
 
 def main():
